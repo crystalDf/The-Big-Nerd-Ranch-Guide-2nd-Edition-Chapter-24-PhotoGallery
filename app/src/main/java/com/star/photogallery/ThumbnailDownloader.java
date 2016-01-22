987c99378cbd84f4ef6728065b7756096e6b031c
crystalDf <chendong333@gmail.com>
2016-01-22 21:42:40 +0800

    Chapter 24 PhotoGallery

---
+++ app/src/main/java/com/star/photogallery/ThumbnailDownloader.java
@@ -0,0 +1,18 @@
+package com.star.photogallery;
+
+import android.os.HandlerThread;
+import android.util.Log;
+
+
+public class ThumbnailDownloader<T> extends HandlerThread {
+
+    private static final String TAG = "ThumbnailDownloader";
+
+    public ThumbnailDownloader() {
+        super(TAG);
+    }
+
+    public void queueThumbnail(T target, String url) {
+        Log.i(TAG, "Got a URL " + url);
+    }
+}
