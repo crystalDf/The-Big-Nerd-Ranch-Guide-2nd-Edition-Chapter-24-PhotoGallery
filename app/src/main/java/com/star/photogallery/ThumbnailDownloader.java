287f154dcc332a044622da08be0123004d3bc1be
crystalDf <chendong333@gmail.com>
2016-01-26 20:44:14 +0800

    Chapter 24 PhotoGallery

--- app/src/main/java/com/star/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/star/photogallery/ThumbnailDownloader.java
@@ -1,12 +1,21 @@
 package com.star.photogallery;
 
+import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 
 public class ThumbnailDownloader<T> extends HandlerThread {
 
     private static final String TAG = "ThumbnailDownloader";
+
+    private static final int MESSAGE_DOWNLOAD = 0;
+
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
 
     public ThumbnailDownloader() {
         super(TAG);
